db68deb352c6105c4ac73c9252a2ecc0aa1f975d
Greg Bacchus <greg.bacchus@directed.co.nz>
2023-11-30 18:31:16 +1100

    fix lint

--- packages/@jexop/date-time/src/now.ts
+++ packages/@jexop/date-time/src/now.ts
@@ -1,4 +1,4 @@
-import { Operator } from '@jexop/core';
+import type { Operator } from '@jexop/core';
 import { DateTime } from 'luxon';
 
 export const nowOperator: Operator = () => DateTime.utc();
