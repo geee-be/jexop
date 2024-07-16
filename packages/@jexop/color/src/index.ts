eb2b4fc89b515700abba60f5166a641e13a326cf
aswebdev <andrew.schadendorff@gmail.com>
2024-07-16 10:21:25 +1000

    Addition of color alpha operator to color operators

--- packages/@jexop/color/src/index.ts
+++ packages/@jexop/color/src/index.ts
@@ -1,5 +1,7 @@
 import { colorRangeOperator } from './color-range.js';
+import { colorAlphaOperator } from './color-alpha.js';
 
 export const colorOperators = {
+  'color-alpha': colorAlphaOperator,
   'color-range': colorRangeOperator,
 };
