fedc5d8c9624da17ca009f42e3d485463154e0d1
aswebdev <andrew.schadendorff@gmail.com>
2024-07-17 07:40:49 +1000

    Minor function naming update

--- packages/@jexop/color/src/color-alpha.ts
+++ packages/@jexop/color/src/color-alpha.ts
@@ -1,7 +1,7 @@
 import { isNumber, isString, toNumber, type Operator } from '@jexop/core';
 import chroma from 'chroma-js';
 
-export const alphaOperator: Operator<string> = ({ color, opacity }) => {
+export const colorAlphaOperator: Operator<string> = ({ color, opacity }) => {
   const alpha = toNumber(opacity);
   if (alpha === null) return null;
 
