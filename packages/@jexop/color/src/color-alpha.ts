1e08922512ab0bd454c57ceabcffc6efbdf54b15
aswebdev <andrew.schadendorff@gmail.com>
2024-07-16 10:17:40 +1000

    Addition of alpha color channel operator

---
+++ packages/@jexop/color/src/color-alpha.ts
@@ -0,0 +1,13 @@
+import { isNumber, isString, toNumber, type Operator } from '@jexop/core';
+import chroma from 'chroma-js';
+
+export const alphaOperator: Operator<string> = ({ color, opacity }) => {
+  const alpha = toNumber(opacity);
+  if (alpha === null) return null;
+
+  const setColor: chroma.Color = isString(color) || isNumber(color) ? chroma(color) : chroma('#000000');
+
+  const alphaColor = chroma(setColor).alpha(opacity);
+  return setColor.hex();
+
+};
